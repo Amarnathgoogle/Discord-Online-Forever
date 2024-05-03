c2b26fb46efb38ba7de8c585ac5f10bd656e07fb
Amarnathgoogle <151923577+Amarnathgoogle@users.noreply.github.com>
2024-05-04 01:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`);
+});
+
+bot.on("messageCreate", (msg) => {
+  if (msg.content === "!ping") {
+    msg.channel.createMessage("Pong!");
+  }
+});
+
 bot.connect(); // Get the bot to connect to Discord
